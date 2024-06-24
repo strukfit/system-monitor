323ab4852a5188483a082602532f3d0cac46e0e4
strukfit <strukfit0@gmail.com>
2024-06-24 15:01:22 +0200

    Add project files.

---
+++ SystemMonitor/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QMainWindow>
+
+
+int main(int argc, char** argv)
+{
+	QApplication app(argc, argv);
+
+	QMainWindow* window = new QMainWindow();
+    window->show();
+
+	return app.exec();
+}
