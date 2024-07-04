7eed1db8edc4555c12f05613255f5ab619ca5b81
strukfit <strukfit0@gmail.com>
2024-07-04 14:28:07 +0200

    perf: split the update of each device into separate thread

--- SystemMonitor/main.cpp
+++ SystemMonitor/main.cpp
@@ -24,7 +24,74 @@ QLabel* memLabel;
 QLabel* diskLabel;
 QLabel* gpuLabel;
 
-void updateDisksAsync(DisksInfo& disksInfo, QLabel* diskLabel)
+static void updateCPUAsync(CPUInfo& cpuInfo, QLabel* cpuLabel)
+{
+    cpuInfo.updateInfo();
+    QString labelText = QString(
+        "CPU_NAME: %1\n"
+        "CPU_USAGE: %2\n"
+        "CPU_PROCESSES: %3\n"
+        "CPU_THREADS: %4\n"
+        "CPU_HANDLES: %5\n"
+        "CPU_BASE_SPEED: %6 GHz\n"
+        "CPU_CORES: %7\n"
+        "CPU_LOGIC_PROC: %8\n")
+        .arg(cpuInfo.modelName())
+        .arg(cpuInfo.usage())
+        .arg(cpuInfo.processCount())
+        .arg(cpuInfo.threadCount())
+        .arg(cpuInfo.handleCount())
+        .arg(cpuInfo.baseSpeed())
+        .arg(cpuInfo.coreCount())
+        .arg(cpuInfo.logicalProcessorCount());
+
+    QMetaObject::invokeMethod(cpuLabel, "setText", Qt::QueuedConnection, Q_ARG(QString, labelText));
+}
+
+static void updateMEMAsync(MEMInfo& memInfo, QLabel* memLabel)
+{
+    memInfo.updateInfo();
+    QString labelText = QString(
+        "MEM: %1/%2 GB\n"
+        "MEM_AVAIL: %3\n"
+        "MEM_PAGE_FILE: %4/%5 GB\n"
+        "MEM_PAGE_FILE_AVAIL: %6\n"
+        "MEM_SPEED: %7 MHz")
+        .arg(memInfo.usedGB())
+        .arg(memInfo.totalGB())
+        .arg(memInfo.availGB())
+        .arg(memInfo.usedPageFileGB())
+        .arg(memInfo.totalPageFileGB())
+        .arg(memInfo.availPageFileGB())
+        .arg(memInfo.speedMHz());
+
+    QMetaObject::invokeMethod(memLabel, "setText", Qt::QueuedConnection, Q_ARG(QString, labelText));
+}
+
+static void updateGPUsAsync(GPUsInfo& gpusInfo, QLabel* gpuLabel)
+{
+    gpusInfo.updateInfo();
+    QString labelText;
+    for (const auto& gpu : gpusInfo.allGPUs())
+    {
+        QString gpuText = QString(
+            "GPU_NAME: %1\n"
+            "GPU_USAGE: %2 %\n"
+            "GPU_MEM_USAGE: %3/%4 Gb\n"
+            "GPU_TEMPERATURE: %5 C\n\n")
+            .arg(gpu->modelName())
+            .arg(gpu->usage() / 1.f)
+            .arg(gpu->memoryUsed() / 1024.f / 1024.f / 1024.f)
+            .arg(gpu->memoryTotal() / 1024.f / 1024.f / 1024.f)
+            .arg(gpu->temperature());
+
+        labelText.append(gpuText);
+    }
+
+    QMetaObject::invokeMethod(gpuLabel, "setText", Qt::QueuedConnection, Q_ARG(QString, labelText));
+}
+
+static void updateDisksAsync(DisksInfo& disksInfo, QLabel* diskLabel)
 {
     disksInfo.updateInfo();
     QString labelText;
@@ -64,60 +131,14 @@ GPUsInfo gpusInfo;
 
 Q_SLOT void updateLabels()
 {
-    cpuInfo.updateInfo();
+    std::thread updateCPUThread(updateCPUAsync, std::ref(cpuInfo), cpuLabel);
+    updateCPUThread.detach();
 
-    cpuLabel->setText(QString(
-        "CPU_NAME: %1\n"
-        "CPU_USAGE: %2\n"
-        "CPU_PROCESSES: %3\n"
-        "CPU_THREADS: %4\n"
-        "CPU_HANDLES: %5\n"
-        "CPU_BASE_SPEED: %6 GHz\n"
-        "CPU_CORES: %7\n"
-        "CPU_LOGIC_PROC: %8\n")
-        .arg(cpuInfo.modelName())
-        .arg(cpuInfo.usage())
-        .arg(cpuInfo.processCount())
-        .arg(cpuInfo.threadCount())
-        .arg(cpuInfo.handleCount())
-        .arg(cpuInfo.baseSpeed())
-        .arg(cpuInfo.coreCount())
-        .arg(cpuInfo.logicalProcessorCount()));
+    std::thread updateMEMThread(updateMEMAsync, std::ref(memInfo), memLabel);
+    updateMEMThread.detach();
 
-    memInfo.updateInfo();
-    memLabel->setText(QString(
-        "MEM: %1/%2 GB\n"
-        "MEM_AVAIL: %3\n"
-        "MEM_PAGE_FILE: %4/%5 GB\n"
-        "MEM_PAGE_FILE_AVAIL: %6\n"
-        "MEM_SPEED: %7 MHz")
-        .arg(memInfo.usedGB())
-        .arg(memInfo.totalGB())
-        .arg(memInfo.availGB())
-        .arg(memInfo.usedPageFileGB())
-        .arg(memInfo.totalPageFileGB())
-        .arg(memInfo.availPageFileGB())
-        .arg(memInfo.speedMHz()));
-
-    gpusInfo.updateInfo();
-    QString gpuLabelText;
-    for (const auto& gpu : gpusInfo.allGPUs())
-    {
-        QString gpuText = QString(
-            "GPU_NAME: %1\n"
-            "GPU_USAGE: %2 %\n"
-            "GPU_MEM_USAGE: %3/%4 Gb\n"
-            "GPU_TEMPERATURE: %5 C\n\n")
-            .arg(gpu->modelName())
-            .arg(gpu->usage() / 1.f)
-            .arg(gpu->memoryUsed() / 1024.f / 1024.f / 1024.f)
-            .arg(gpu->memoryTotal() / 1024.f / 1024.f / 1024.f)
-            .arg(gpu->temperature());
-
-        gpuLabelText.append(gpuText);
-    }
-
-    gpuLabel->setText(gpuLabelText);
+    std::thread updateGPUsThread(updateGPUsAsync, std::ref(gpusInfo), gpuLabel);
+    updateGPUsThread.detach();
 
     std::thread updateDisksThread(updateDisksAsync, std::ref(disksInfo), diskLabel);
     updateDisksThread.detach();
