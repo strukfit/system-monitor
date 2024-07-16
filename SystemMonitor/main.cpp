936b99e3b4580bba379adcc39941f59225180f81
Rostyslav Borysenko <strukfit0@gmail.com>
2024-07-16 22:36:47 +0200

    fix: disk chart was fixed for Linux

--- SystemMonitor/main.cpp
+++ SystemMonitor/main.cpp
@@ -1,12 +1,15 @@
 #include <QApplication>
 #include "MainWindow.h"
 
+using ulonglong = unsigned long long;
 
 int main(int argc, char** argv)
 {
     qputenv("QT_DEBUG_PLUGINS", "1");
     QApplication app(argc, argv);
     app.setStyle("Fusion");
+
+    qRegisterMetaType<ulonglong>("ulonglong");
 
     MainWindow window;
     window.show();
